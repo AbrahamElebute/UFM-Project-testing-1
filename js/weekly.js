5d4ebd12a8937b8bdd683dc2721547ba124f3170
AbrahamElebute <elebuteimisioluwa@gmail.com>
2022-01-02 00:12:09 +0100

    UFM Project testing 1

---
+++ js/weekly.js
@@ -0,0 +1,58 @@
+< !DOCTYPE html >
+    <
+    html lang = "en" >
+
+    <
+    head >
+    <
+    meta charset = "UTF-8" >
+    <
+    meta http - equiv = "X-UA-Compatible"
+content = "IE=edge" >
+    <
+    meta name = "viewport"
+content = "width=device-width, initial-scale=1.0" >
+    <
+    link rel = "stylesheet"
+href = "css/weekly.css" >
+    <
+    title > Document < /title> < /
+    head >
+
+    <
+    body >
+    <
+    h1 > Responsive Card < /h1> <
+div class = "wrapper" >
+    <
+    div class = "card" >
+    <
+    h3 class = "card-title" > SUNDAYS < /h3> <
+p class = "card-content" > Worship Hour < br > 8 am - 11 am < /p> < /
+    div > <
+    div class = "card" >
+    <
+    h3 class = "card-title" > TUESDAYS < /h3> <
+p class = "card-content" > Word season < br > 6: 30 pm - 8 pm < /p> < /
+    div > <
+    div class = "card" >
+    <
+    h3 class = "card-title" > CARD TITLE < /h3> <
+p class = "card-content" > Contrary to popular belief, Lorem Ipsum is not simply random text.It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. < /p> < /
+    div > <
+    div class = "card" >
+    <
+    h3 class = "card-title" > CARD TITLE < /h3> <
+p class = "card-content" > Contrary to popular belief, Lorem Ipsum is not simply random text.It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. < /p> < /
+    div > <
+    div class = "card" >
+    <
+    h3 class = "card-title" > CARD TITLE < /h3> <
+p class = "card-content" > Contrary to popular belief, Lorem Ipsum is not simply random text.It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. < /p> <
+button class = "card-btn" > Go Home < /button> < /
+    div > <
+    /div> < /
+    body >
+
+    <
+    /html>
